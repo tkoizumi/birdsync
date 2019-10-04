0ccc33cfc89416e02fc93dc5adf74c50b157ae0d
Taka Koizumi <takayuki.koizumi@gmail.com>
2019-10-04 14:26:39 -0400

    added server code

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-console.log('Hello Hello');
+console.log('Hello Hello Starting Server');
 //hello
+
+
+// content of index.js
+const http = require('http')
+const port = 8080;
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
